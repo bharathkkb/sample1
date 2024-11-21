73f068c589dc72c46fb7b6927238fa5953acf98a
Bharath KKB <bharathkrishnakb@gmail.com>
2024-11-21 12:39:42 -0600

    Update var.tf

--- testvars/var.tf
+++ testvars/var.tf
@@ -2,3 +2,8 @@ variable "foo" {
   description = "The ID of the project where this VPC will be created"
   type        = string
 }
+
+variable "bar" {
+  description = "The ID of the project where this VPC will be created"
+  type        = string
+}
